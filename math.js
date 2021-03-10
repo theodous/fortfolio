286c97e4168b762c3951a3bff9e0ed4839ec8645
theodous <dato155@gmail.com>
2021-03-10 20:11:44 +0400

    Add files via upload

---
+++ math.js
@@ -0,0 +1,59 @@
+function sum(a,b) {
+   
+   if(typeof a=="string" && typeof b=="string" )
+   {
+       return "please give us a number";
+
+
+
+   } 
+
+   return a+b;
+
+
+}
+
+function minus(a,b) {
+
+    if(typeof a=="string" && typeof b=="string" )
+    {
+        return "please give us a number";
+ 
+ 
+ 
+    } 
+
+   return a-b;
+
+
+}
+
+function sub(a,b) {
+
+    if(typeof a=="string" && typeof b=="string" )
+    {
+        return "please give us a number";
+ 
+ 
+ 
+    } 
+
+    return a*b;
+ 
+ 
+ }
+
+ function div(a,b) {
+
+    if(typeof a=="string" && typeof b=="string" )
+    {
+        return "please give us a number";
+ 
+ 
+ 
+    } 
+
+    return a/b;
+ 
+ 
+ }
